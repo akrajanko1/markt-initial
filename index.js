8e600fe3167cb035e9d592c2e18147f50dec1654
akrajanko1 <33953668+akrajanko1@users.noreply.github.com>
2018-01-05 10:49:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const async = require('async');
 const restService = express();
 const JSON = require('circular-json');
 var db = mongoose.connect('mongodb://akr:akr@ds239127.mlab.com:39127/markt');
-var User = require('./models/userModel');
+var User1 = require('./models/userModel');
+Schema = mongoose.Schema;
+var modelName="categories";
+var defaultModel = new Schema({})
+var User = db.model(modelName,defaultModel);
 restService.use(
   bodyParser.urlencoded({
     extended: true
